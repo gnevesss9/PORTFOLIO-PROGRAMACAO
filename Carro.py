b99d9ae8d0d3fd0f2b7687db0fe33bf0a3b3ea1d
Guilherme Neves <g.nevess2024@gmail.com>
2025-09-17 12:25:58 +0100

    Carro.py

--- Carro.py
+++ Carro.py
@@ -1,4 +1,5 @@
 class Carro:
+    #Aqui é o construtor da classe, que inicializa os atributos do carro
     def __innit__(self, marca, modelo, ano, cor):
         self.marca = marca
         self.modelo = modelo
@@ -6,18 +7,21 @@ class Carro:
         self.cor = cor
 
     def mostrar_info(self):
-        print(f"\nCarro: {self.marca} {self.modelo})")
+        #Mostra as informações do carro, como marca, modelo, ano e cor
+        print(f"\nCarro: {self.marca} {self.modelo}")
         print(f"Ano: {self.ano} | Cor: {self.cor}")
 
 print("=== Cria o teu carro ===")
+# Solicita ao usuário que insira os detalhes do carro, como marca, modelo, ano e cor
 marca = input("Marca: ")
 modelo = input("Modelo: ")
 ano = input("Ano: ")
 cor = input("Cor: ")
 
 meu_carro = Carro(marca, modelo, ano, cor)
+# Guarda as informações do carro criado na variável meu_carro
 
 meu_carro.mostrar_info()
+# Exibe as informações do carro criado
 
 print("\n Carro criado com sucesso!")
-    
